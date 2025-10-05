05b698a8f86b84527498058a775e4aba5d8744c4
Erfan Moosavi <erfanmoosavi84@gmail.com>
2025-10-05 18:17:46 +0330

    Is truth a woman?

---
+++ Codes/02_value_of_truth.py
@@ -0,0 +1,10 @@
+# The problem of the millennia
+
+while True:
+    answer = input("Does the truth matter?\n")
+    if answer == "Yes":
+        continue
+    elif answer == "No":
+        break
+
+print("Nothing is real, but trees are beautiful")
